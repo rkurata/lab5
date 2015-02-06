fe0d2e2f81efffa3de8f220c8c7e7473bff50901
vagrant <vagrant@precise32.(none)>
2015-02-06 08:20:39 +0000

    putting it together in lab

--- public/js/hci-friends.js
+++ public/js/hci-friends.js
@@ -1,15 +1,26 @@
 'use strict';
 
+	
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
 	initializePage();
-})
+});
 
 /*
  * Function that is called when the document is ready.
  */
 function initializePage() {
 	console.log("Javascript connected!");
+	$("a").click(projectClick);
+	//$('friends.name').click(function(e){
+	//	e.preventDefault();
+	//	$(this).text(anagrammedName($(this).text()));
+	//});
+}
+
+function projectClick(e) {
+	e.preventDefault();
+	$(this).text(anagrammedName($(this).text()));
 }
 
 function anagrammedName(name) {
--- routes/add.js
+++ routes/add.js
@@ -2,4 +2,14 @@ var data = require("../data.json");
 
 exports.addFriend = function(req, res) {    
 	// Your code goes here
+	//console.log(data);
+	res.render('add');
+	var newFriend= 
+ {
+ 'name': req.query.name,
+ 'description': req.query.description,
+ 'imageURL': 'http://lorempixel.com/400/400/people'
+ };
+ data["friends"].push(newFriend);
+ //console.log(newFriend);
  }
